cbf36cc0765d349796d7fb599d14f26eef0cd5ac
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-11 12:28:06 +0200

    add media

--- app-basketball/src/modules/teams/components/TeamCard/TeamCard.tsx
+++ app-basketball/src/modules/teams/components/TeamCard/TeamCard.tsx
@@ -8,7 +8,7 @@ export const TeamCard: FC = () => {
     return (
         <Flex >
             <Logo >
-                <ImgWrapper><Img src={team}   /></ImgWrapper>
+                <Img src={team}   />
             </Logo>
             <CartItem >
                 <Name>Portland trail blazers</Name>
@@ -31,41 +31,22 @@ const Logo = styled.div`
   align-items: center;
   background: linear-gradient(121.57deg, #707070 1.62%, #393939 81.02%);
   border-radius: 4px 4px 0 0;
-  max-width: 520px;
+  max-width: 365px;
   width: 100%;
   height: 280px;
-  @media (max-width: 1000px) {
-    height: 220px;
-  }
-  @media (max-width: 768px) {
-    height: 140px;
+  @media (max-width: 400px) {
+    height: 150px;
   }
 `
 
-const ImgWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 150px;
-  height: 150px;
-  overflow: hidden;
-  @media (max-width: 768px) {
-    width: 120px;
-    height: 120px;
-  }
-  @media(max-width: 768px) {
-    width: 100px;
-    height: 100px;
-  }
-  @media (max-width: 360px) {
-    width: 70px;
-    height: 70px;
-  }
-`
 
 const Img = styled.img`
+  max-width: 150px;
   width: 100%;
-  object-fit: cover
+  object-fit: cover;
+  @media (max-width: 400px) {
+    width: 80px;
+  }
 `
 
 const CartItem = styled.div`
@@ -73,7 +54,7 @@ const CartItem = styled.div`
   align-items: center;
   flex-direction: column;
   background-color: ${({theme}) => theme.colors.darkGrey};
-  max-width: 520px;
+  max-width: 365px;
   width: 100%;
   padding: 24px 0;
   border-radius: 0 0 4px 4px;
@@ -83,7 +64,7 @@ const Name = styled.h3`
   font-weight: 500;
   font-size: 18px;
   line-height: 25px;
-  @media (max-width: 768px) {
+  @media (max-width: 400px) {
     font-size: 15px;
   }
 `
@@ -91,7 +72,7 @@ const Yarn = styled.p`
   color: ${({theme}) => theme.colors.lightGrey};
   margin-top: 10px;
   text-align: center;
-  @media (max-width: 768px) {
+  @media (max-width: 400px) {
     font-size: 12px;
   }
 `
