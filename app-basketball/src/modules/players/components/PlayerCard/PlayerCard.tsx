aef7c24a42259926ece974a5c7ccef94c480896c
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-11 12:27:44 +0200

    add media

--- app-basketball/src/modules/players/components/PlayerCard/PlayerCard.tsx
+++ app-basketball/src/modules/players/components/PlayerCard/PlayerCard.tsx
@@ -30,21 +30,29 @@ const Logo = styled.div`
   align-items: flex-end;
   background: linear-gradient(121.57deg, #707070 1.62%, #393939 81.02%);
   border-radius: 4px 4px 0 0;
-  max-width: 520px;
+  max-width: 365px;
   width: 100%;
   height: 280px;
-
+  overflow: hidden;
+  @media (max-width: 400px) {
+    height: 150px;
+  }
 `
+
 const Img = styled.img`
-  width:70%;
-  object-fit: cover
+  width:90%;
+  height: 100%;
+  object-fit: cover;
+  @media (max-width: 400px) {
+  width: 150px;
+    height: 120px;
+}
 `
-
 const CartItem = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  max-width: 520px;
+  max-width: 365px;
   width: 100%;
   background-color: ${({theme}) => theme.colors.darkGrey};
   padding: 24px 0;
@@ -54,7 +62,8 @@ const Name = styled.h3`
   color: white;
   font-weight: 500;
   font-size: 18px;
-  @media (max-width: 768px) {
+  line-height: 25px;
+  @media (max-width: 400px) {
     font-size: 15px;
   }
 `
@@ -65,7 +74,7 @@ const Number = styled.span`
 const Teams = styled.p`
   color: ${({theme}) => theme.colors.lightGrey};
   margin-top: 10px;
-  @media (max-width: 768px) {
+  @media (max-width: 400px) {
     font-size: 12px;
   }
 `
