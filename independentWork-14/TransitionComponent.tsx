5de9d5798ea19bdb969cb27264bc9489c0abee0e
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-12 18:48:39 +0200

    classWork

---
+++ independentWork-14/TransitionComponent.tsx
@@ -0,0 +1,14 @@
+export const TransitionComponent = () => {
+  return (
+    <>
+      <div className="transition">
+        <input className={""} type={"checkbox"} />
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <p className="">Магия вне хогвартса</p>
+      </div>
+    </>
+  );
+};
