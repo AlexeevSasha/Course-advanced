46c9ad90f5aa878914f8130ae4f9d04292134109
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-14 20:29:15 +0200

    add form player

---
+++ app-basketball/src/modules/players/components/FormPlayer/FormPlayer.tsx
@@ -0,0 +1,109 @@
+import React, {FC} from "react";
+import styled from "styled-components";
+import {InputFile, Input, ButtonCancel, Button, Selects} from "../../../../common/components";
+import {optionsPosition} from "../../../../common/components/Select/data";
+
+
+export const FormPlayer: FC = () => {
+    return (
+        <Wrapper>
+            <BreadCrumbs>BreadCrumbs</BreadCrumbs>
+            <Form>
+                <InputFileContainer>
+                    <InputFile/>
+                </InputFileContainer>
+                <FormRight>
+                    <Input title="Name"/>
+                    <div>
+                        <SelectTitle>Position</SelectTitle>
+                        <Selects options={optionsPosition}/>
+                    </div>
+                    <div>
+                        <SelectTitle>Teams</SelectTitle>
+                        <Selects options={[]}/>
+                    </div>
+                    <Grid>
+                    <Input title="Height (cm)" type='number'/>
+                    <Input title="Weight (kg)" type='number'/>
+                    <Input title="Birthday"  />
+                    <Input title="Number" type='number'/>
+                    </Grid>
+                    <BtnWrapper>
+                        <ButtonCancel type='button'/>
+                        <Button type='submit'>Save</Button>
+                    </BtnWrapper>
+                </FormRight>
+            </Form>
+        </Wrapper>
+    )
+}
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 24px;
+`
+
+const SelectTitle = styled.label`
+color: ${({ theme }) => theme.colors.grey};
+font-size: 14px;
+display: block;
+margin-bottom: 5px;
+`
+const InputFileContainer = styled.div`
+  max-width: 500px;
+  width: 100%;
+  @media ${({theme}) => theme.media._980} {
+    display: flex;
+    justify-content: center;
+  };
+`
+
+const Wrapper = styled.div`
+  background: white;
+  border-radius: 10px;
+  padding-bottom: 48px;
+`
+const BreadCrumbs = styled.h4`
+  color: ${({theme}) => theme.colors.red};
+  font-weight: 500;
+  padding: 24px 0 0 32px;
+  cursor: pointer;
+  @media ${({theme}) => theme.media._768} {
+    padding: 16px 0 0 16px;
+  };
+`
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  margin: 72px 72px 0 72px;
+
+  & > :first-child {
+    margin-right: 48px;
+  }
+
+  @media ${({theme}) => theme.media._980} {
+    margin: 24px 24px 0 24px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    & > :first-child {
+      margin: 0 0 24px;
+    }
+  };
+`
+const FormRight = styled.div`
+  display: grid;
+  gap: 24px;
+  max-width: 366px;
+  width: 100%;
+`
+const BtnWrapper = styled.div`
+  display: flex;
+
+  & > :last-child {
+    margin-left: 24px;
+  }
+`
