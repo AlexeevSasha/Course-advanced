24583ae7368f28c9db6a06ebf03cb979e7a432d4
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-14 20:16:35 +0200

    add export files

---
+++ app-basketball/src/common/components/index.tsx
@@ -0,0 +1,19 @@
+export {Button} from './Button/Button';
+export {ButtonCancel} from './Button/ButtonCancel';
+export {Header} from './Header/Header';
+export {HeaderCardInfo} from './HeaderCardInfo/HeaderCardInfo';
+export {Input} from './Input/Input';
+export {InputCheckbox} from './Input/InputCheckbox';
+export {InputFile} from './Input/InputFile'
+export {Search} from './Input/Search';
+export {NavMenu} from './NavMenu/NavMenu';
+export {Pagination} from './Pagination/Pagination';
+export {Selects} from './Select/Select';
+
+
+//icons / imageSvg
+export {ChekMarkIcon, ProfileIcon, ArrowRigthIcon, ArrowLeftIcon,
+        CreateIcon, DeleteIcon, EyeIcon,  MenuIcon, CloseEyeIcon,
+        PlayersIcon, SearchIcon, SingOutIcon, TeamsIcon, InputFileIcon,
+        Error404Svg, EmptyTeamsSvg, SingInSvg, SingUpSvg, EmptyPlayersSvg
+        }from './SVGConstans/SVG'
