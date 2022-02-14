4ea1a1fe89e1fa422680f4ed4eb6e5e4197e8d52
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-14 20:35:37 +0200

    add router

--- app-basketball/src/index.tsx
+++ app-basketball/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from "styled-components";
+import {ThemeProvider} from "styled-components";
 import {theme} from "./assets/style/theme";
+import {BrowserRouter} from "react-router-dom";
 import {App} from './App';
 import './assets/style/style.css'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-    <App />
-      </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <App/>
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
