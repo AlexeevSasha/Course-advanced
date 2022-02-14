ba725c518a72bebb863c53de42bc20b9eaa515b2
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-14 20:11:24 +0200

    add burger menu

--- app-basketball/src/common/components/Header/Header.tsx
+++ app-basketball/src/common/components/Header/Header.tsx
@@ -1,59 +1,64 @@
-import React, { FC } from "react";
+import React, {FC} from "react";
 import logo from '../../../assets/images/header-logo.png';
-import profileIcon from '../../../assets/icons/profile.svg';
-import menuIcon from '../../../assets/icons/menu.svg';
+import {MenuIcon, ProfileIcon} from "../";
 import styled from "styled-components";
 
-
-
-interface Props  {
+interface Props {
     name?: string;
+    toggle?: boolean;
+    clickToggle?: () => void;
 };
 
-export const Header: FC<Props> = ({name }) => {
+export const Header: FC<Props> = ({name, toggle, clickToggle}) => {
     return (
-            <HeaderWrapper >
-                <HeaderMenu src={menuIcon} alt=""/>
-                <HeaderLogo >
-                   <LogoImg src={logo} alt="logo" />
-                </HeaderLogo>
-                <HeaderProfile >
-                    <HeaderName >{name || 'Name'}</HeaderName>
-                    <HeaderAvatar src={profileIcon} alt="" />
-                </HeaderProfile>
-            </HeaderWrapper>
+        <HeaderWrapper>
+            <HeaderMenu toggle={toggle}  onClick={clickToggle}><MenuIcon/></HeaderMenu>
+            <HeaderLogo>
+                <LogoImg src={logo} alt="logo"/>
+            </HeaderLogo>
+            <HeaderProfile>
+                <HeaderName>{name || 'Name'}</HeaderName>
+                <HeaderAvatar><ProfileIcon/></HeaderAvatar>
+            </HeaderProfile>
+        </HeaderWrapper>
     )
 }
 
 
 const HeaderWrapper = styled.div`
-position: relative;
-display: flex;
-justify-content: space-between;
-background-color: white;
-padding: 0 50px;box-shadow: 0px 1px 10px rgba(209, 209, 209, 0.5);
-width: 100%;
-height: 80px;
-@media (max-width: 600px) {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 0 50px;
+  box-shadow: 0 1px 10px rgba(209, 209, 209, 0.5);
+  width: 100%;
+  height: 80px;
+  @media ${({theme}) => theme.media._768} {
     justify-content: center;
     height: 62px;
-}
+  }
 `
 
 const HeaderProfile = styled.div`
-display: flex;
-align-items: center;
-@media (max-width: 600px) {
+  display: flex;
+  align-items: center;
+  @media ${({theme}) => theme.media._768} {
     display: none;
-}
+  }
 `
 const HeaderName = styled.span`
-color: ${({ theme }) => theme.colors.darkGrey};
-margin-right: 16px;
+  color: ${({theme}) => theme.colors.darkGrey};
+  margin-right: 16px;
 `
 
-const HeaderAvatar = styled.img`
-width: 40px;
+const HeaderAvatar = styled.div`
+  height: 40px;
+
+  & > svg {
+    width: 40px;
+    height: 40px;
+  }
 `
 const HeaderLogo = styled.div`
   display: flex;
@@ -63,20 +68,30 @@ const HeaderLogo = styled.div`
 
 const LogoImg = styled.img`
   width: 100%;
-  @media (max-width: 600px){
+  @media ${({theme}) => theme.media._768} {
     width: 150px;
   }
 `
 
-const HeaderMenu = styled.img`
-display: none;
-@media (max-width: 600px) {
+const HeaderMenu = styled.button<{toggle?: boolean}>`
+  display: none;
+  @media ${({theme}) => theme.media._768} {
     display: block;
     position: absolute;
+    padding: 0;
+    border: none;
+    height: 24px;
+    background: transparent;
     left: 12px;
     cursor: pointer;
     top: 50%;
+    border-radius: 50%;
+    transition: all .2s linear;
     transform: translateY(-50%);
+    ${({toggle}) => toggle && `
+    transform: translateY(-50%)  rotate(90deg);
+    `}
+  }
 }
 `
 
