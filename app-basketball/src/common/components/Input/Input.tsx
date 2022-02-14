de8e40d20ee7973479152e0ee2332cb773e49cd4
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-14 20:13:08 +0200

    add eye type password

--- app-basketball/src/common/components/Input/Input.tsx
+++ app-basketball/src/common/components/Input/Input.tsx
@@ -1,58 +1,87 @@
 import React, {FC, InputHTMLAttributes} from "react";
+import {EyeIcon, CloseEyeIcon} from "../";
 import styled from "styled-components";
 
-interface IInput extends InputHTMLAttributes<HTMLInputElement>{
-    error?:  string;
+
+interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+    error?: string;
+    type?: string;
+    title?: string;
+    date?: boolean;
 }
-export const Input :FC<IInput> = (({error, ...attr}) => {
+
+export const Input: FC<IInput> = (({title, type = 'text', error, date, ...attr}) => {
+    const [typeInput, setTypeInput] = React.useState<string>(type);
+    const showHide = () => {
+        setTypeInput(typeInput === "password" ? "text" : "password");
+    }
     return (
-        <InputWrapper >
-            <LabelStyle>Label</LabelStyle>
-            <InputStyle error={error} {...attr}/>
-            {error ? <ErrorStyle >{error}</ErrorStyle> : ''}
-        </InputWrapper>
+        <div>
+            <LabelStyle>{title}</LabelStyle>
+            <Container>
+                <InputStyle type={typeInput} error={error} {...attr} date={date}/>
+                {type === 'password' &&
+                    <IconStyle onClick={showHide}>{typeInput === 'password' ? <CloseEyeIcon/> : <EyeIcon/>}</IconStyle>}
+            </Container>
+            {error ? <ErrorStyle>{error}</ErrorStyle> : ''}
+        </div>
     )
 })
-const InputWrapper = styled.div`
-margin-bottom: 24px;
+
+const IconStyle = styled.div`
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  right: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`
+const Container = styled.div`
+  display: inline;
+  position: relative;
 `
 const LabelStyle = styled.label`
-color: ${({ theme }) => theme.colors.grey};
-font-size: 14px;
-display: block;
-margin-bottom: 5px;
+  color: ${({theme}) => theme.colors.grey};
+  font-size: 14px;
+  display: block;
+  margin-bottom: 5px;
 `
-const InputStyle = styled.input<{error?: string}>`
-background-color: ${({ theme }) => theme.colors.lightestGrey1};
-max-width: 366px;
-width: 100%;
-height: 40px;
-border: ${({ error, theme }) => error ? `1px solid ${theme.colors.lightestRed}` : `1px solid ${theme.colors.lightestGrey1}`};
-border-radius: 4px;
-color: ${({ theme }) => theme.colors.darkGrey};
-font-family: inherit;
-font-weight: 500;
-padding: 0 30px 0 12px;
-font-size: 14px; 
+const InputStyle = styled.input<IInput>`
+  background-color: ${({theme}) => theme.colors.lightestGrey1};
+  max-width: 366px;
+  width: 100%;
+  height: 40px;
+  border: ${({
+               error,
+               theme
+             }) => error ? `1px solid ${theme.colors.lightestRed}` : `1px solid ${theme.colors.lightestGrey1}`};
+  border-radius: 4px;
+  color: ${({theme}) => theme.colors.darkGrey};
+  font-family: inherit;
+  font-weight: 500;
+  padding: 0 30px 0 12px;
+  font-size: 14px;
 
-&:hover {
-    background-color:  ${({ theme }) => theme.colors.lightestGrey};
-    border: 1px solid ${({ theme }) => theme.colors.lightestGrey};
-}
+  &:hover {
+    background-color: ${({theme}) => theme.colors.lightestGrey};
+    border: 1px solid ${({theme}) => theme.colors.lightestGrey};
+  }
 
-&:focus {
-    background-color: ${({ theme }) => theme.colors.lightestGrey1};
-    border: 1px solid ${({ theme }) => theme.colors.lightestGrey1};
+  &:focus {
+    background-color: ${({theme}) => theme.colors.lightestGrey1};
+    border: 1px solid ${({theme}) => theme.colors.lightestGrey1};
     box-shadow: 0 0 5px #D9D9D9;
-}
-&:disabled {
-  pointer-events: none;
-  color: ${({ theme }) => theme.colors.lightestGrey};
-}
+  }
+
+  &:disabled {
+    pointer-events: none;
+    color: ${({theme}) => theme.colors.lightestGrey};
+  }
 `
 const ErrorStyle = styled.p`
-font-size: 12px;
-font-weight: 500;
-color: ${({ theme }) => theme.colors.lightestRed};
-margin-top: 4px;
+  font-size: 12px;
+  font-weight: 500;
+  color: ${({theme}) => theme.colors.lightestRed};
+  margin-top: 4px;
 `
