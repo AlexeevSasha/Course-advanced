2f701a7dd3c5190b79907de8214306d73021bc51
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-21 14:10:19 +0200

    add adaptive styles

--- app-basketball/src/modules/players/components/PlayerCard/PlayerCard.tsx
+++ app-basketball/src/modules/players/components/PlayerCard/PlayerCard.tsx
@@ -7,7 +7,7 @@ export const PlayerCard: FC = () => {
     return (
         <Flex>
             <Logo>
-                <Img src={player} alt=""/>
+             <ImgContainer> <Img src={player} alt=""/></ImgContainer>
             </Logo>
             <CartItem>
                 <Name>Jaylen Adams<Number>#10</Number></Name>
@@ -17,10 +17,20 @@ export const PlayerCard: FC = () => {
     )
 }
 
+
 const Flex = styled.div`
   display: flex;
   flex-direction: column;
   cursor: pointer;
+`
+const  ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  max-width: 280px;
+  width: 100%;
+ height: 100%;
+  overflow: hidden;
 `
 
 const Logo = styled.div`
@@ -29,29 +39,27 @@ const Logo = styled.div`
   align-items: flex-end;
   background: linear-gradient(121.57deg, #707070 1.62%, #393939 81.02%);
   border-radius: 4px 4px 0 0;
-  //max-width: 365px;
+  max-width: 365px;
   width: 100%;
   height: 280px;
-  overflow: hidden;
-  @media (max-width: 400px) {
+  @media (max-width: 1100px) {
+    height: 230px;
+  }
+  @media ${({theme}) => theme.media._480} {
     height: 150px;
   }
 `
 
 const Img = styled.img`
   width: 100%;
-  height: 100%;
-  object-fit: contain;
-  @media (max-width: 400px) {
-    width: 150px;
-    height: 120px;
-  }
+  height: 90%;
+  object-fit: cover;
 `
 const CartItem = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  //max-width: 365px;
+  max-width: 365px;
   width: 100%;
   background-color: ${({theme}) => theme.colors.darkGrey};
   padding: 24px 0;
@@ -62,7 +70,7 @@ const Name = styled.h3`
   font-weight: 500;
   font-size: 18px;
   line-height: 25px;
-  @media (max-width: 400px) {
+  @media ${({theme}) => theme.media._480} {
     font-size: 15px;
   }
 `
@@ -73,7 +81,7 @@ const Number = styled.span`
 const Teams = styled.p`
   color: ${({theme}) => theme.colors.lightGrey};
   margin-top: 10px;
-  @media (max-width: 400px) {
+  @media ${({theme}) => theme.media._480} {
     font-size: 12px;
   }
 `
