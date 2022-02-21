14ef06a5f2223957c1515cbb195764e551122984
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-21 14:06:23 +0200

    add validation

--- app-basketball/src/modules/authorization/components/SingUpForm.tsx
+++ app-basketball/src/modules/authorization/components/SingUpForm.tsx
@@ -1,23 +1,35 @@
-import React, { FC } from "react";
+import React, {FC, useState} from "react";
 import { Input, Button, InputCheckbox } from "../../../common/components";
 import {Container,  FormContainer, Title, Form, Text, TextLink} from './style'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import {IFormRegistration, RegistrationSchema} from "./schemaValidation";
+import {signUp} from "../../../api/auth/authService";
 
 
 export const SingUpForm: FC = () => {
+    const [isChecked, setIsChecked] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm<IFormRegistration>({
+        resolver: yupResolver(RegistrationSchema)
+    });
+    const onSubmit = (data: IFormRegistration) => {
+        const {login, userName, password} = data;
+        signUp({login, userName, password})
+    };
     return (
             <Container >
                 <FormContainer >
                     <Title >Sign Up</Title>
-                    <Form >
-                        <Input title={'Name'}/>
-                        <Input title={'Login'}/>
-                        <Input title={'Password'} type="password"  />
-                        <Input title={'Enter your password again'} type="password"  />
-                        <InputCheckbox/>
-                        <Button>Sign Up</Button>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Input id='nameUp' title={'Name'} {...register('userName')} error={errors.userName?.message} />
+                        <Input  id='loginUp' title={'Login'} {...register('login')} error={errors.login?.message}/>
+                        <Input id='passwordUp' title={'Password'} type="password" {...register('password')} error={errors.password?.message}/>
+                        <Input id='passwAgain' title={'Enter your password again'} type="password" {...register('confirmPassword')} error={errors.confirmPassword?.message} />
+                        <InputCheckbox value={isChecked} onChange={setIsChecked}/>
+                        <Button disabled={!isChecked}>Sign Up</Button>
                     </Form>
                     <Text >
-                        Already a member? <TextLink >Sign In</TextLink>
+                        Already a member? <TextLink to='/login' >Sign In</TextLink>
                     </Text>
                 </FormContainer>
             </Container>
