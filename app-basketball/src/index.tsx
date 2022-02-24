05ccd452023bc5abfd6d3f1b082272d872c0a594
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 15:01:02 +0200

    add store

--- app-basketball/src/index.tsx
+++ app-basketball/src/index.tsx
@@ -5,15 +5,19 @@ import {theme} from "./assets/style/theme";
 import {BrowserRouter} from "react-router-dom";
 import {App} from './App';
 import './assets/style/style.css'
+import {Provider} from "react-redux";
+import {store} from "./core/redux/store";
 
 
 ReactDOM.render(
     <React.StrictMode>
+        <Provider store={store}>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
                 <App/>
             </ThemeProvider>
         </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
