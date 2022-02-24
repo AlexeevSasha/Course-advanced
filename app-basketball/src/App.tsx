dca58666a7d9c4553b8f23ff160d1859b108829e
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 15:00:45 +0200

    add pivate provider

--- app-basketball/src/App.tsx
+++ app-basketball/src/App.tsx
@@ -9,22 +9,25 @@ import {AddPlayer} from "./modules/players/pages/AddPlayer";
 import {SingIn} from "./modules/authorization/pages/SingIn";
 import {SingUp} from "./modules/authorization/pages/SingUp";
 import {InfoPlayer} from './modules/players/pages/InfoPlayer';
-import {InfoTeam} from './modules/teams/pages/InfoTeam'
+import {InfoTeam} from './modules/teams/pages/InfoTeam';
+import {AuthProvider} from "./common/hooks/AuthProvider";
 
 
 export const App = () => {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/teams"/>}/>
             <Route path="login" element={<SingIn/>}/>
             <Route path="registration" element={<SingUp/>}/>
-            <Route path="/" element={<Navigate to="/login"/>}/>
-            <Route path='/' element={<OutletWrapper/>}>
-                <Route path='teams' element={<Teams/>}/>
-                <Route path='teams/:id' element={<InfoTeam/>}/>
-                <Route path='teams/addTeam' element={<AddTeam/>} />
-                <Route path='players' element={<Players/>}/>
-                <Route path='players/:id' element={<InfoPlayer/>}/>
-                <Route path='players/addPlayer' element={<AddPlayer/>} />
+            <Route  element={<AuthProvider/>}>
+                <Route path='/' element={<OutletWrapper/>}>
+                    <Route path='teams' element={<Teams/>}/>
+                    <Route path='teams/:id' element={<InfoTeam/>}/>
+                    <Route path='teams/addTeam' element={<AddTeam/>}/>
+                    <Route path='players' element={<Players/>}/>
+                    <Route path='players/:id' element={<InfoPlayer/>}/>
+                    <Route path='players/addPlayer' element={<AddPlayer/>}/>
+                </Route>
             </Route>
             <Route path='*' element={<Error404/>}/>
         </Routes>
