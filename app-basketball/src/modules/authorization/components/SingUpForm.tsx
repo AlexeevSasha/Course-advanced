3cdfa2ba150d03f7e0783a522af0e15e44234753
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:49:25 +0200

    add request regist

--- app-basketball/src/modules/authorization/components/SingUpForm.tsx
+++ app-basketball/src/modules/authorization/components/SingUpForm.tsx
@@ -1,30 +1,44 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import { Input, Button, InputCheckbox } from "../../../common/components";
 import {Container,  FormContainer, Title, Form, Text, TextLink} from './style'
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import {IFormRegistration, RegistrationSchema} from "./schemaValidation";
-import {signUp} from "../../../api/auth/authService";
+import {IFormRegistration} from "./interfaces";
+import {useNavigate} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../core/redux/reduxType";
+import {registerAction} from "../authorizationAction";
 
 
 export const SingUpForm: FC = () => {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const { user} = useAppSelector(state => state.auth)
     const [isChecked, setIsChecked] = useState(false);
-    const { register, handleSubmit, formState: { errors } } = useForm<IFormRegistration>({
-        resolver: yupResolver(RegistrationSchema)
-    });
+    const { register, handleSubmit,watch, formState: { errors } } = useForm<IFormRegistration>();
+    const passwordCurrent = watch("password", "");
     const onSubmit = (data: IFormRegistration) => {
         const {login, userName, password} = data;
-        signUp({login, userName, password})
+        dispatch(registerAction({login, userName, password}))
     };
+    useEffect(() => {
+        if (user) navigate('/teams', { replace: true })
+    }, [user])
     return (
             <Container >
                 <FormContainer >
                     <Title >Sign Up</Title>
                     <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Input id='nameUp' title={'Name'} {...register('userName')} error={errors.userName?.message} />
-                        <Input  id='loginUp' title={'Login'} {...register('login')} error={errors.login?.message}/>
-                        <Input id='passwordUp' title={'Password'} type="password" {...register('password')} error={errors.password?.message}/>
-                        <Input id='passwAgain' title={'Enter your password again'} type="password" {...register('confirmPassword')} error={errors.confirmPassword?.message} />
+                        <Input id='nameUp' title={'Name'} {...register('userName',{ required: 'Name required'})} error={errors.userName?.message} />
+                        <Input  id='loginUp' title={'Login'} {...register('login', { required: 'Login required'})} error={errors.login?.message}/>
+                        <Input id='passwordUp' title={'Password'} type="password" {...register('password', { required: 'Password required',
+                            minLength : {
+                                value: 3,
+                                message: 'Password must contain at least 3 characters'
+                            }
+                        })} error={errors.password?.message}/>
+                        <Input id='passwAgain' title={'Enter your password again'} type="password" {...register('confirmPassword',
+                            { required: 'Confirm password',
+                                validate: value =>value === passwordCurrent || "The passwords do not match"
+                        })} error={errors.confirmPassword?.message} />
                         <InputCheckbox value={isChecked} onChange={setIsChecked}/>
                         <Button disabled={!isChecked}>Sign Up</Button>
                     </Form>
