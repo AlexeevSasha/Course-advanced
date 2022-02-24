302bd1b2a6e43481fe51eeb71812a2013410473f
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:41:49 +0200

    edit baseRequest

--- app-basketball/src/api/baseRequest.ts
+++ app-basketball/src/api/baseRequest.ts
@@ -2,14 +2,31 @@ export const BASE_URL = 'http://dev.trainee.dex-it.ru/'
 
 interface IBaseRequestProps {
     url: string;
-    method: string
+    method: "GET" | "POST" | "PUT" | "DELETE";
     headers?: {
         "Content-Type"?: "application/json" | "multipart/form-data";
         Authorization?: string;
     },
-    body?: string  ;
+    body?: string;
+    isAuth: boolean;
 }
 
 export const baseRequest = async ({ url, ...rest }: IBaseRequestProps) => {
-    return await fetch(BASE_URL + url, { ...rest });
+    try {
+        const user =  JSON.parse(`${localStorage.getItem("user")}`);
+        const auth = rest.isAuth ? { Authorization: `Bearer ${user.token}`} : {};
+        const response = await fetch(BASE_URL + url, { ...rest, headers: {...rest.headers, ...auth}});
+        if (response.status === 401) {
+            throw new Error('Wrong login or password')
+        }
+        if (response.status === 409) {
+            throw new Error('User already registered')
+        }
+        if (!response.ok) {
+            throw new Error(response.status.toString())
+        }
+        return response.json()
+    } catch (e : any) {
+        throw new Error(e.message);
+    }
 };
