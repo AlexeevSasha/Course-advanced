c6e82a9df72eb55e09d76ada5121db2a53960434
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:49:11 +0200

    add request login

--- app-basketball/src/modules/authorization/components/SingInForm.tsx
+++ app-basketball/src/modules/authorization/components/SingInForm.tsx
@@ -1,26 +1,40 @@
-import React, { FC } from "react";
+import React, {FC, useEffect} from "react";
 import { Input, Button } from "../../../common/components";
 import {Container,  FormContainer, Title, Form, Text, TextLink} from './style';
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import {IFormLogin, LoginSchema} from "./schemaValidation";
-import {signIn} from "../../../api/auth/authService";
+import {useNavigate} from "react-router-dom";
+import {IFormLogin} from "./interfaces";
+import {useAppDispatch, useAppSelector} from "../../../core/redux/reduxType";
+import {loginAction} from "../authorizationAction";
+
 
 
 export const SingInForm: FC = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<IFormLogin>({
-        resolver: yupResolver(LoginSchema)
-    });
-    const onSubmit = (data: IFormLogin) => {
-        signIn(data)
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const { user} = useAppSelector(state => state.auth)
+    const { register, handleSubmit, formState: { errors } } = useForm<IFormLogin>();
+    const onSubmit =  (data: IFormLogin) => {
+         dispatch(loginAction(data))
     };
+    useEffect(() => {
+        if (user) navigate('/teams', { replace: true })
+    }, [user])
     return (
             <Container >
                 <FormContainer >
                     <Title >Sign In</Title>
                     <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Input id='loginIn' title={'Login'}  {...register('login')} error={errors.login?.message}/>
-                        <Input id='passwordIn' title={'Password'} type="password"  {...register('password')} error={errors.password?.message}/>
+                        <Input id='loginIn' title={'Login'}
+                               {...register('login', { required: 'Login required'})}
+                               error={errors.login?.message}/>
+                        <Input id='passwordIn' title={'Password'} type="password"
+                               {...register('password', { required: 'Password required',
+                                   minLength : {
+                                       value: 3,
+                                       message: 'Password must contain at least 3 characters'
+                                   }
+                               })} error={errors.password?.message}/>
                         <Button>Sign In</Button>
                     </Form>
                     <Text >
