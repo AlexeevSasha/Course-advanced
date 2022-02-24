60f86ddd4c3225d71b74e4f91aede331f597f103
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:45:27 +0200

    add auth provider

---
+++ app-basketball/src/common/hooks/AuthProvider.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import {Outlet} from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import {useAppSelector} from "../../core/redux/reduxType";
+
+
+
+export const AuthProvider: FC = () => {
+    const {user} = useAppSelector(state=>state.auth);
+    if (!user) {
+        return <Navigate to="/login" replace/>;
+    }
+    return  <Outlet/>;
+}
