56d0762f6c99f22757bdef761370b7259fbbebe2
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:39:58 +0200

    edit authService

--- app-basketball/src/api/auth/authService.ts
+++ app-basketball/src/api/auth/authService.ts
@@ -1,43 +1,22 @@
 import {baseRequest} from "../baseRequest";
 import {ISignIn, ISignUp} from "./authDto";
 
-export const signIn = async (data:ISignIn) => {
-    try {
-        const response = await baseRequest({
-            url: "api/Auth/SignIn",
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        })
-        if (response.status === 401) {
-            throw new Error('Wrong login or password')
-        }
-        if (!response.ok) {
-            throw new Error(response.status.toString())
-        }
-
-        console.log(response.json())
-    } catch (error : any) {
-        console.log(error.message)
-    }
+export const signIn = (data: ISignIn) => {
+    return baseRequest({
+        url: "api/Auth/SignIn",
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+        isAuth: false
+    })
 }
 
-export const signUp = async (data:ISignUp) => {
-    try {
-        const response = await baseRequest({
-            url: "api/Auth/SignUp",
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        })
-        if (response.status === 409) {
-            throw new Error('User already registered')
-        }
-        if (!response.ok) {
-            throw new Error(response.status.toString())
-        }
-        return response.json()
-    } catch (error : any) {
-        console.log(error.message)
-    }
+export const signUp = (data: ISignUp) => {
+    return baseRequest({
+        url: "api/Auth/SignUp",
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+        isAuth: false
+    })
 }
