4948c6de8e7b6a00000c5ed4768de7091b874a46
AlexeevSasha <work.alexeevs@gmail.com>
2022-02-24 14:54:03 +0200

    add dispatch

--- app-basketball/src/modules/players/pages/AddPlayer.tsx
+++ app-basketball/src/modules/players/pages/AddPlayer.tsx
@@ -1,7 +1,13 @@
-import React, {FC} from "react";
+import React, {FC, useEffect} from "react";
 import {FormPlayer} from "../components/FormPlayer/FormPlayer";
+import {useAppDispatch} from "../../../core/redux/reduxType";
+import {teamOptionAction} from "../../teams/teamsAction";
 
 export const AddPlayer : FC = () => {
+    const dispath = useAppDispatch()
+    useEffect(() => {
+        dispath(teamOptionAction())
+    }, [dispath])
     return (
         <FormPlayer/>
     )
