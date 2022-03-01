42a0e9539e75bdb122ba9bcc0ded6f88178a8860
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-01 09:15:17 +0200

    add pages

--- app-basketball/src/App.tsx
+++ app-basketball/src/App.tsx
@@ -11,6 +11,8 @@ import {SingUp} from "./modules/authorization/pages/SingUp";
 import {InfoPlayer} from './modules/players/pages/InfoPlayer';
 import {InfoTeam} from './modules/teams/pages/InfoTeam';
 import {AuthProvider} from "./common/hooks/AuthProvider";
+import {EditTeam} from "./modules/teams/pages/EditTeam";
+import {EditPlayer} from "./modules/players/pages/EditPlayer";
 
 
 export const App = () => {
@@ -24,9 +26,12 @@ export const App = () => {
                     <Route path='teams' element={<Teams/>}/>
                     <Route path='teams/:id' element={<InfoTeam/>}/>
                     <Route path='teams/addTeam' element={<AddTeam/>}/>
+                    <Route path="teams/:id/edit" element={<EditTeam/>}/>
                     <Route path='players' element={<Players/>}/>
                     <Route path='players/:id' element={<InfoPlayer/>}/>
+                    <Route path="players/:id/edit" element={<EditPlayer/>}/>
                     <Route path='players/addPlayer' element={<AddPlayer/>}/>
+
                 </Route>
             </Route>
             <Route path='*' element={<Error404/>}/>
