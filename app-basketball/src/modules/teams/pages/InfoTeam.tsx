437245d972ffb6b287d7c2e0f8c70b71104a22a4
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-01 09:13:59 +0200

    add request team and spinner

--- app-basketball/src/modules/teams/pages/InfoTeam.tsx
+++ app-basketball/src/modules/teams/pages/InfoTeam.tsx
@@ -1,7 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC,useState} from 'react';
 import {TeamInfo} from "../components/TeamInfo/TeamInfo";
+import {HeaderCardInfo, PopapDelete} from "../../../common/components";
+import {deleteTeamThunk} from "../teamsAction";
+import {useAppDispatch} from "../../../core/redux/reduxType";
+import {useNavigate, useParams} from "react-router-dom";
+
 export const InfoTeam: FC = () => {
+    const navigate = useNavigate()
+    const dispatch = useAppDispatch()
+    const { id } = useParams();
+    const [visible, setVisible] = useState<boolean>(false)
+    const onHandlerClickYes = () => {
+        setVisible(false);
+        dispatch(deleteTeamThunk(Number(id)))
+    }
+    const onEditTeam = () => navigate('edit')
+    const onDeleteTeam = () => setVisible(true)
+    const closeModal = () => setVisible(false)
     return (
+        <div style={{position: 'relative'}}>
+            <PopapDelete visible={visible} onClose={closeModal} onHandlerClickYes={onHandlerClickYes}/>
+            <HeaderCardInfo onDelete={onDeleteTeam} onEdit={onEditTeam}/>
         <TeamInfo/>
+        </div>
     );
 }
