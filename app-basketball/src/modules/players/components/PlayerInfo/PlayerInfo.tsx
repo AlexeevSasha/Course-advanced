936e43be89d8524a016c1f656bd20971d9caf682
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-01 09:09:25 +0200

    add request info player

--- app-basketball/src/modules/players/components/PlayerInfo/PlayerInfo.tsx
+++ app-basketball/src/modules/players/components/PlayerInfo/PlayerInfo.tsx
@@ -1,51 +1,65 @@
-import React, {FC, InputHTMLAttributes} from "react";
-import {HeaderCardInfo} from "../../../../common/components/HeaderCardInfo/HeaderCardInfo";
-import player from '../../../../assets/images/player.png'
+import React, {FC, useEffect} from "react";
 import styled from "styled-components";
+import noLogo from '../../../../assets/images/no-logo-player.svg'
+import {useAppDispatch, useAppSelector} from "../../../../core/redux/reduxType";
+import {Spinner} from "../../../../common/components";
+import {useParams} from "react-router-dom";
+import {getPlayerIdThunk} from "../../playersAction";
+import {differenceInYears } from 'date-fns';
+
+
 
 
 export const PlayerInfo:FC = () => {
+    const dispatch = useAppDispatch()
+    const { id } = useParams();
+    const {player, loadingPlayers} = useAppSelector(state => state.players)
+    useEffect(() => {
+        dispatch(getPlayerIdThunk(Number(id)))
+    }, [])
     return (
-        <div>
-        <HeaderCardInfo/>
         <PlayerWrapper>
-           <ImgLogo><img  src={player} alt='logo'/></ImgLogo>
+            {loadingPlayers ?<Spinner/> : <>
+           <ImgLogo><img  src={player?.avatarUrl || noLogo} alt='logo'/></ImgLogo>
             <Content>
-                <Name>Greg Whittington <Number>#22</Number></Name>
+
+                <Name>{player?.name} <Numbers>#{player?.number}</Numbers></Name>
                 <Flex>
                     <Group>
                         <Wrapper>
                             <Title>Position</Title>
-                            <Text>Forward</Text>
+                            <Text>{player?.position}</Text>
                         </Wrapper>
                         <Wrapper>
                             <Title>Team</Title>
-                            <Text>Denver Nuggets</Text>
+                            <Text>{player?.teamName}</Text>
                         </Wrapper>
                     </Group>
                     <Group>
                         <Wrapper>
                             <Title>Height</Title>
-                            <Text>206 cm</Text>
+                            <Text>{player?.height} cm</Text>
                             </Wrapper>
                          <Wrapper>
                              <Title>Weight</Title>
-                              <Text>95 kg</Text>
+                              <Text>{player?.weight} kg</Text>
                          </Wrapper>
                     </Group>
                     <Group>
 
                         <Wrapper> <Title>Age</Title>
-                            <Text>27</Text>
+                            <Text>{differenceInYears(new Date().getTime(),new Date(player?.birthday || '')) || 0}</Text>
                         </Wrapper>
 
                     </Group>
                 </Flex>
             </Content>
+            </>
+            }
         </PlayerWrapper>
-        </div>
     )
 }
+
 
 const PlayerWrapper = styled.div`
   display: flex;
@@ -54,31 +68,33 @@ const PlayerWrapper = styled.div`
   border-radius: 0 0 10px 10px;
   align-items: center;
   height: 525px;
-  @media (max-width: 700px) {
+  @media ${({theme}) => theme.media._980} {
     flex-direction: column;
-    height: auto;
+    height: 100%;
+    margin-bottom: 20px;
   }
 `
 const ImgLogo = styled.div`
-  max-width: 750px;
+  max-width: 600px;
   width: 100%;
   height: 100%;
   display: flex;
   align-items: flex-end;
   justify-content: center;
   overflow: hidden;
-  @media  (max-width: 1000px) {
+  @media  ${({theme}) => theme.media._980} {
     max-width: 350px;
   }
   img {
     width: 500px;
     height: 400px;
-    object-fit: cover;
-    @media  (max-width: 1000px) {
+    object-fit: contain;
+    @media  ${({theme}) => theme.media._980} {
       width: 300px;
       height: 250px;
+      margin: 48px auto;
     }
-    @media  (max-width: 700px) {
+    @media  ${({theme}) => theme.media._768} {
       margin: 48px auto;
       width: 250px;
       height: 150px;
@@ -91,7 +107,7 @@ const Content = styled.div`
   align-items: flex-start;
   flex-direction: column;
   color: white;
-  @media (max-width: 700px) {
+  @media ${({theme}) => theme.media._980} {
     align-items: center;
     margin: 0;
   }
@@ -102,16 +118,16 @@ const Name = styled.div`
   font-size: 36px;
   line-height: 49px;
   margin-bottom: 40px;
-  @media  (max-width: 1000px) {
+  @media  ${({theme}) => theme.media._980} {
     font-size: 30px;
     line-height: 35px;
   }
-  @media (max-width: 500px) {
+  @media ${({theme}) => theme.media._480} {
     font-size: 20px;
     line-height: 25px;
   }
 `
-const Number = styled.span`
+const Numbers = styled.span`
   color: ${({theme}) => theme.colors.lightRed};
 `
 const Flex = styled.div`
@@ -122,7 +138,7 @@ const Flex = styled.div`
 const Group = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  @media (max-width: 700px) {
+  @media ${({theme}) => theme.media._980} {
     grid-template-columns: repeat(1, 1fr);
     align-items: center;
     justify-items: center;
@@ -130,7 +146,7 @@ const Group = styled.div`
 `
 const Wrapper = styled.div`
   margin-right: 80px;
-  @media (max-width: 700px) {
+  @media ${({theme}) => theme.media._980} {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -143,7 +159,7 @@ const Title = styled.div`
   font-size: 24px;
   line-height: 33px;
 
-  @media (max-width: 500px) {
+  @media ${({theme}) => theme.media._480} {
     font-size: 17px;
     line-height: 20px;
     margin: 0;
@@ -156,10 +172,10 @@ const Text = styled.div`
   line-height: 25px;
   margin-bottom: 50px;
   margin-top: 8px;
-  @media (max-width: 700px) {
+  @media ${({theme}) => theme.media._980} {
     margin-bottom: 32px;
   }
-  @media (max-width: 500px) {
+  @media ${({theme}) => theme.media._980} {
     font-size: 15px;
     line-height: 17px;
   }
