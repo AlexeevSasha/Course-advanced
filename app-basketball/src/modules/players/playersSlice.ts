23908c754ee1c3ffcbbfb73475261dbf784b72b9
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-03 12:24:44 +0200

    add output error

--- app-basketball/src/modules/players/playersSlice.ts
+++ app-basketball/src/modules/players/playersSlice.ts
@@ -1,6 +1,6 @@
 import {createSlice} from "@reduxjs/toolkit";
 import { IGetPlayerResponse} from "../../api/players/playersDto";
-import {getPlayersThunk , addPlayersThunk, getPlayerIdThunk, editPlayersThunk, deletelayersThunk } from "./playersAction";
+import {getPlayersThunk , addPlayersThunk, getPlayerIdThunk, editPlayersThunk, deletePlayersThunk } from "./playersAction";
 
 
 interface ITeams {
@@ -14,7 +14,7 @@ interface IState {
     players: ITeams | null;
     player: IGetPlayerResponse | null;
     loadingPlayers: boolean;
-    errorPlayers: boolean;
+    errorPlayers: string | undefined;
 }
 
 
@@ -22,7 +22,7 @@ const initialState: IState = {
     players: null,
     player: null,
     loadingPlayers: false,
-    errorPlayers: false,
+    errorPlayers: '',
 };
 
 export const playersSlice = createSlice({
@@ -33,68 +33,68 @@ export const playersSlice = createSlice({
         //getPlayers
         builder.addCase(getPlayersThunk .pending, (state) => {
             state.loadingPlayers = true;
+            state.errorPlayers = '';
         });
         builder.addCase(getPlayersThunk .fulfilled, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = false;
             state.players = action.payload
         });
-        builder.addCase(getPlayersThunk .rejected, (state) => {
+        builder.addCase(getPlayersThunk .rejected, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = true;
+            state.errorPlayers = action.error.message;
         });
 
         //getPlayerId
         builder.addCase(getPlayerIdThunk .pending, (state) => {
             state.loadingPlayers = true;
+            state.errorPlayers = '';
         });
         builder.addCase(getPlayerIdThunk .fulfilled, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = false;
             state.player = action.payload
         });
-        builder.addCase(getPlayerIdThunk .rejected, (state) => {
+        builder.addCase(getPlayerIdThunk .rejected, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = true;
+            state.errorPlayers = action.error.message;
         });
 
         //addPlayers
         builder.addCase(addPlayersThunk .pending, (state) => {
             state.loadingPlayers = true;
+            state.errorPlayers = '';
         });
         builder.addCase(addPlayersThunk .fulfilled, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = false;
         });
-        builder.addCase(addPlayersThunk .rejected, (state) => {
+        builder.addCase(addPlayersThunk .rejected, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = true;
+            state.errorPlayers = action.error.message;
         });
 
         //editPlayers
         builder.addCase(editPlayersThunk .pending, (state) => {
             state.loadingPlayers = true;
+            state.errorPlayers = '';
         });
         builder.addCase(editPlayersThunk .fulfilled, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = false;
         });
-        builder.addCase(editPlayersThunk .rejected, (state) => {
+        builder.addCase(editPlayersThunk .rejected, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = true;
+            state.errorPlayers = action.error.message;
         });
 
         //deletePlayers
-        builder.addCase(deletelayersThunk .pending, (state) => {
+        builder.addCase(deletePlayersThunk .pending, (state) => {
             state.loadingPlayers = true;
+            state.errorPlayers = '';
         });
-        builder.addCase(deletelayersThunk .fulfilled, (state, action) => {
+        builder.addCase(deletePlayersThunk .fulfilled, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = false;
         });
-        builder.addCase(deletelayersThunk .rejected, (state) => {
+        builder.addCase(deletePlayersThunk .rejected, (state, action) => {
             state.loadingPlayers = false;
-            state.errorPlayers = true;
+            state.errorPlayers = action.error.message;
         });
     }
 })
