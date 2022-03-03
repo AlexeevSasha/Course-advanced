42d55d8ec84c510dfe3af50c402dd77827dca060
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-03 12:26:49 +0200

    add output error

--- app-basketball/src/modules/teams/teamsSlice.ts
+++ app-basketball/src/modules/teams/teamsSlice.ts
@@ -20,7 +20,7 @@ interface IState {
     team: IGetTeams | null;
     teamOption: IOption[];
     loadingTeams: boolean;
-    errorTeams: boolean | string | undefined;
+    errorTeams: string | undefined;
 }
 
 
@@ -29,7 +29,7 @@ const initialState: IState = {
     team: null,
     teamOption: [],
     loadingTeams: false,
-    errorTeams: false,
+    errorTeams: '',
 };
 
 export const teamsSlice = createSlice({
@@ -40,15 +40,15 @@ export const teamsSlice = createSlice({
         //getTeams
         builder.addCase(getTeamsThunk.pending, (state) => {
             state.loadingTeams = true;
+            state.errorTeams = '';
         });
         builder.addCase(getTeamsThunk.fulfilled, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
             state.teams = action.payload;
         });
-        builder.addCase(getTeamsThunk.rejected, (state) => {
+        builder.addCase(getTeamsThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = true;
+            state.errorTeams = action.error.message;
         });
 
         //getTeamId
@@ -58,65 +58,63 @@ export const teamsSlice = createSlice({
         builder.addCase(getTeamIdThunk.fulfilled, (state, action) => {
             state.team = action.payload;
             state.loadingTeams = false;
-            state.errorTeams = false;
         });
-        builder.addCase(getTeamIdThunk.rejected, (state) => {
+        builder.addCase(getTeamIdThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = true;
+            state.errorTeams = action.error.message;
         });
 
         //addteam
         builder.addCase(addTeamThunk.pending, (state) => {
             state.loadingTeams = true;
+            state.errorTeams = '';
         });
-        builder.addCase(addTeamThunk.fulfilled, (state, action) => {
+        builder.addCase(addTeamThunk.fulfilled, (state) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
         });
         builder.addCase(addTeamThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            console.log(action.error.message)
-            state.errorTeams = true;
+            state.errorTeams = action.error.message;
         });
 
         //editteam
         builder.addCase(editTeamThunk.pending, (state) => {
             state.loadingTeams = true;
+            state.errorTeams = '';
         });
-        builder.addCase(editTeamThunk.fulfilled, (state, action) => {
+        builder.addCase(editTeamThunk.fulfilled, (state) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
         });
-        builder.addCase(editTeamThunk.rejected, (state,) => {
+        builder.addCase(editTeamThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
+            state.errorTeams = action.error.message;
         });
 
         //deleteTeam
         builder.addCase(deleteTeamThunk.pending, (state) => {
             state.loadingTeams = true;
+            state.errorTeams = '';
         });
         builder.addCase(deleteTeamThunk.fulfilled, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
         });
-        builder.addCase(deleteTeamThunk.rejected, (state) => {
+        builder.addCase(deleteTeamThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = true;
+            state.errorTeams = action.error.message;
         });
 
         //teamOption
         builder.addCase(teamOptionThunk.pending, (state) => {
             state.loadingTeams = true;
+            state.errorTeams = '';
         });
         builder.addCase(teamOptionThunk.fulfilled, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = false;
             state.teamOption = action.payload.data.map((elem : IGetTeams) => ({value: elem.id, label: elem.name}))
         });
-        builder.addCase(teamOptionThunk.rejected, (state) => {
+        builder.addCase(teamOptionThunk.rejected, (state, action) => {
             state.loadingTeams = false;
-            state.errorTeams = true;
+            state.errorTeams = action.error.message;
         });
     }
 })
