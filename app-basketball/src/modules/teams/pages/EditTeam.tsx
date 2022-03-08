6ab55256f73f01f00f319ba61fcd76cc5f4b6449
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-08 10:18:51 +0200

    add team selector

--- app-basketball/src/modules/teams/pages/EditTeam.tsx
+++ app-basketball/src/modules/teams/pages/EditTeam.tsx
@@ -4,11 +4,12 @@ import { useParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../../core/redux/reduxType";
 import {getTeamIdThunk} from "../teamsAction";
 
+
 export const EditTeam :FC = () => {
     const dispatch = useAppDispatch()
 
     const { id } = useParams();
-    const { team} = useAppSelector((state) => state.teams)
+    const {team} = useAppSelector(state => state.teams)
     useEffect(() => {
         dispatch(getTeamIdThunk(Number(id)))
     },[id])
