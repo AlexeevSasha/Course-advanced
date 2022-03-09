c302f6444fbc1976af792dc5f5d496cd9abcc8dc
AlexeevSasha <work.alexeevs@gmail.com>
2022-03-09 09:19:24 +0200

    classwork

---
+++ independentWork-17/src/App.js
@@ -0,0 +1,12 @@
+import air from './img/air.png'
+import './App.css'
+
+export const App = () => {
+
+  return (
+      <div className='container'>
+        <img className='air' src={air} alt=""/>
+      </div>
+  )
+}
+
